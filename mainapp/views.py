fa3f81d09315ae1b60b3c369e62a7db0dd130b37
Leonardo <lafuenteleonardo99@gmail.com>
2024-02-06 21:11:08 -0400

    superusuario agregado

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,21 +4,21 @@ from django.contrib import messages
 from mainapp.forms import RegisterForm
 from django.contrib.auth import authenticate,login,logout
 from record.models import Company, Area  # Importa los modelos de la empresa y el área
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def index(request):
-    if request.user.is_authenticated:
-        # Obtén las empresas del usuario actual
-        companies = Company.objects.filter(user=request.user)
+    if request.user.is_superuser:
+        # Si el usuario es superusuario, obtén todas las empresas
+        companies = Company.objects.all()
     else:
-        # Si el usuario no está autenticado, no hay empresas para mostrar
-        companies = None
+        # Si el usuario no es superusuario, obtén solo las empresas que le corresponden
+        companies = Company.objects.filter(user=request.user)
 
-    return render(request,'mainapp/index.html',{
-        'title':'Inicio',
+    return render(request, 'mainapp/index.html', {
+        'title': 'Inicio',
         'companies': companies,
     })
-
 
 
 #registro
